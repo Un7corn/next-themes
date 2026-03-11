0f6f6d92ac23c91c04b76afdee84c5ecf5ed0f17
Un7corn <sjd77777djs@gmail.com>
2026-03-11 22:33:25 +0800

    Update index.js

--- examples/example/pages/index.js
+++ examples/example/pages/index.js
@@ -1,16 +1,23 @@
 import { useTheme } from 'next-themes'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import { ThemeToggle } from '../../../components/ThemeToggle'
 
 const Index = () => {
   const { theme, setTheme } = useTheme()
   const [mounted, setMounted] = useState(false)
+
   useEffect(() => setMounted(true), [])
 
   return (
     <div>
       <h1>next-themes Example</h1>
-      <select value={theme} onChange={e => setTheme(e.target.value)} data-test-id="theme-selector">
+
+      <select
+        value={theme}
+        onChange={(e) => setTheme(e.target.value)}
+        data-test-id="theme-selector"
+      >
         <option value="system">System</option>
         {mounted && (
           <>
@@ -23,8 +30,15 @@ const Index = () => {
       <br />
       <br />
 
+      <h2>Theme Toggle Component Demo</h2>
+      <ThemeToggle />
+
+      <br />
+      <br />
+
       <div>
-        <Link href="/dark">Forced Dark Page</Link> • <Link href="/light">Forced Light Page</Link>
+        <Link href="/dark">Forced Dark Page</Link> •{' '}
+        <Link href="/light">Forced Light Page</Link>
       </div>
     </div>
   )
